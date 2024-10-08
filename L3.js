376bd84877dfd4c6a6688c5cc8532774423372f0
BlueDragon320 <vaibhav.b.g.patil@gmail.com>
2024-10-08 21:39:02 +0530

    Completed L3

---
+++ L3.js
@@ -0,0 +1,52 @@
+// function subtract(n){
+//     return n-n
+// }
+
+// function subOfNumbers(a, b){
+//     const val1= Number(a);
+//     const val2= Number(b);
+
+//     return val1 - val2;
+// }
+
+// console.log(subOfNumbers(2,4));
+// The code throwed an error it was becasue of a noob mistake to use Number instead fo number N should to capital letter inorder to work
+
+
+// Thanks to chatgpt heres the short way to do the whole thing
+
+// function subOfNumbers(a, b) {
+//     const val1 = Number(a);  // Use Number() to convert to a number
+//     const val2 = Number(b);
+
+//     return val1 - val2;  // Subtract the two numbers
+// }
+
+// console.log(subOfNumbers(1, 1));  // This will now work correctly
+
+
+
+
+// function square(n){
+//     return n * n
+// }
+// function cube(n){
+//     return  n * n * n
+// }
+
+// function sumOfSomething(a, b, callback){
+
+//     console.log(a);
+//     console.log(callback);
+
+//     const val1 = callback(a)
+//     const val2 = callback(b)
+//     return val1 + val2;
+// }
+
+// const ans = sumOfSomething(2, 2, square)
+// console.log(ans)
+// const ans2 = sumOfSomething(2, 2, cube)
+// console.log(ans2)
+
+
