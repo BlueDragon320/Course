7650195e9c2365115502a9fc357ef553b3a24324
BlueDragon320 <vaibhav.b.g.patil@gmail.com>
2024-10-09 21:46:04 +0530

    partially understood it need to lean again

---
+++ L4.js
@@ -0,0 +1,25 @@
+// function findSum(n){
+//     let ans = 0;
+//     for (let i=0; i<n; i++){
+//         ans += i;
+//     }
+//     return ans;
+// }
+
+// function findSumTill100(){
+//     console.log(findSum(100));
+// }
+
+// // busy waiting
+// function syncsleep(){
+//     let a = 1;
+//     for (let i=0; i<4000000000; i++){
+//         a++;
+//         //console.log(a)
+//     }
+// }
+// syncsleep();
+// findSumTill100();
+// //setTimeout(findSumTill100, 4000)
+// console.log("hello world");
+
